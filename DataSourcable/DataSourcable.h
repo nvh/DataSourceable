9dfb9269eddb68f68ff51e6128adac4a6c2fd26c
Niels van Hoorn <nvh@nvh.io>
2015-10-13 08:44:12 +0200

    Initial Commit

---
+++ DataSourcable/DataSourcable.h
@@ -0,0 +1,19 @@
+//
+//  DataSourcable.h
+//  DataSourcable
+//
+//  Created by Niels van Hoorn on 13/10/15.
+//  Copyright © 2015 Zeker Waar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataSourcable.
+FOUNDATION_EXPORT double DataSourcableVersionNumber;
+
+//! Project version string for DataSourcable.
+FOUNDATION_EXPORT const unsigned char DataSourcableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataSourcable/PublicHeader.h>
+
+
