0650bb13664d596f7b64478d95fbcaacbda3c532
Niels van Hoorn <nvh@nvh.io>
2015-12-29 16:14:40 +0100

    Refactoring Loadable to use primitive Result enum

--- DataSourceable/Loadable.swift
+++ DataSourceable/Loadable.swift
@@ -6,22 +6,27 @@
 //  Copyright © 2015 Zeker Waar. All rights reserved.
 //
 
+public enum Result<Value, E> {
+    case Success(Value)
+    case Failure(E)
+}
+
 public protocol Loadable: class {
     associatedtype Data : EmptyCheckable
     var state: State<Data,ErrorType> { get set }
-    func loadData(completion: (Data) -> Void) throws
+    func loadData(completion: (Result<Data,ErrorType>) -> Void)
 }
 
 public extension Loadable {
     func reload(completion: () -> Void) {
         state = state.toLoading()
-        do {
-            try loadData { data in
+        loadData { result in
+            switch result {
+            case .Success(let data):
                 self.state = self.state.toReady(data)
-                completion()
+            case .Failure(let error):
+                self.state = self.state.toError(error)
             }
-        } catch {
-            self.state = self.state.toError(error)
             completion()
         }
     }
