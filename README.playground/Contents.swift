0664c93bf5326e02968669063ddede83d84af04e
Niels van Hoorn <nvh@nvh.io>
2016-05-27 08:54:28 +0100

    Updated playground to refactored Configurable

--- README.playground/Contents.swift
+++ README.playground/Contents.swift
@@ -7,6 +7,7 @@ import XCPlayground
 let defaultReuseIdentifier = "default"
 
 struct MovieTitleDataSource: TableViewDataSourceable, DataContaining {
+    
     typealias Data = [String]
     typealias Section = Data
     var data: Data? = ["Casino Royale","Quantum of Solace","Skyfall","Spectre"]
@@ -14,13 +15,20 @@ struct MovieTitleDataSource: TableViewDataSourceable, DataContaining {
     func reuseIdentifier(forIndexPath indexPath: NSIndexPath) -> String {
         return defaultReuseIdentifier
     }
-    
-    func configure(cell cell: UITableViewCell, forItem item: String, inView view: UITableView) -> UITableViewCell {
-        cell.textLabel?.text = item
-        return cell
+}
+
+//This could also be your custom subclass of UITableViewCell
+extension UITableViewCell: Configurable {
+    public typealias ItemType = String
+    public func configure(forItem item: ItemType, inView view: ContainingViewType) {
+        textLabel?.text = item
     }
 }
 
+//Specify the class you would like this tableview to use here
+extension MovieTitleDataSource: TableViewCellProviding {
+    typealias TableViewCellType = UITableViewCell
+}
 
 let tableView: UITableView = UITableView(frame: CGRect(x: 0, y: 0, width: 320, height: 500))
 let dataSource = MovieTitleDataSource()
